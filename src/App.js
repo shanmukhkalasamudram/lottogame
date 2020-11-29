b67cd0304488b8c7df45e4e0a4cd05bb339c5154
shanmukhkalasamudram <shanmukhkalasamudram@gmail.com>
2020-11-29 07:27:36 +0530

    app-deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Lottery from "./Lottery";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Lottery title='Mega Contest' />
+        <Lottery title='Mini Daily' maxNum={10} numBalls={4} />
+      
     </div>
   );
 }
